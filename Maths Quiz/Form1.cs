c4c0ab7bab4d70c7aa6e7a582daca6869a1eabaf
danjocutler <danjocutler@gmail.com>
2015-03-19 16:11:23 +0000

    initial setup of form completed

---
+++ Maths Quiz/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maths_Quiz
+{
+    public partial class form1 : Form
+    {
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void product_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
