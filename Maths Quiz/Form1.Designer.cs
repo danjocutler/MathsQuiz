6988590e4f5093ac1f7f34d34567cd0ccdd81cb3
danjocutler <danjocutler@gmail.com>
2015-03-20 14:03:21 +0000

    Added random number generator and StartTheQuiz method

--- Maths Quiz/Form1.Designer.cs
+++ Maths Quiz/Form1.Designer.cs
@@ -279,6 +279,7 @@
             this.startButton.TabIndex = 1;
             this.startButton.Text = "Start the quiz!";
             this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
             // form1
             // 
--- Maths Quiz/Form1.cs
+++ Maths Quiz/Form1.cs
@@ -12,6 +12,23 @@ namespace Maths_Quiz
 {
     public partial class form1 : Form
     {
+
+        Random randomiser = new Random();
+
+        int addend1;
+        int addend2;
+
+        public void StartTheQuiz()
+        {
+            addend1 = randomiser.Next(51);
+            addend2 = randomiser.Next(51);
+
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+
+            sum.Value = 0;
+        }
+
         public form1()
         {
             InitializeComponent();
@@ -25,6 +42,12 @@ namespace Maths_Quiz
         private void product_ValueChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
         }
     }
 }
